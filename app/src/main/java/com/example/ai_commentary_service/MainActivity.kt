4985c609c5245b30b7bbdfc8e13fde1c4c627453
user <ikmin7373@naver.com>
2023-12-07 18:02:55 +0900

    it's just for test

---
+++ app/src/main/java/com/example/ai_commentary_service/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.ai_commentary_service
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ai_commentary_service.ui.theme.AI_Commentary_ServiceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AI_Commentary_ServiceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AI_Commentary_ServiceTheme {
+        Greeting("Android")
+    }
+}
